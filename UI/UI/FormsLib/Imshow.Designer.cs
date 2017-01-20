c53233aaa7c9afde1ba9b826f3caec450a23bbc5
Syed Nizamuddin Peeran <Syed Nizamuddin@DESKTOP-16T0583>
2017-01-21 00:28:38 +0530

    1.Added MenuStrip items. 2.added open and save functionality 3.Need to add comments 4.Added Functionality to to detect pixel color at a given pint in rgb 5.Need to work on other colour formats 6.GetColorAt method is incomplete. 7.Save as and open need corrections in file formats

--- UI/UI/FormsLib/Imshow.Designer.cs
+++ UI/UI/FormsLib/Imshow.Designer.cs
@@ -28,19 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStrip = new System.Windows.Forms.ToolStrip();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripPixelInfo = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip
             // 
             this.menuStrip.AccessibleRole = System.Windows.Forms.AccessibleRole.MenuBar;
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolsToolStripMenuItem});
             this.menuStrip.Location = new System.Drawing.Point(0, 0);
             this.menuStrip.Name = "menuStrip";
             this.menuStrip.Size = new System.Drawing.Size(525, 24);
             this.menuStrip.TabIndex = 0;
             this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "SaveAs";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
             // 
             // toolStrip
             // 
@@ -53,29 +98,70 @@
             // 
             // statusStrip
             // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripPixelInfo});
             this.statusStrip.Location = new System.Drawing.Point(0, 351);
             this.statusStrip.Name = "statusStrip";
             this.statusStrip.Size = new System.Drawing.Size(525, 22);
             this.statusStrip.TabIndex = 2;
             this.statusStrip.Text = "statusStrip";
             // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 49);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(525, 302);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // toolStripPixelInfo
+            // 
+            this.toolStripPixelInfo.Name = "toolStripPixelInfo";
+            this.toolStripPixelInfo.Size = new System.Drawing.Size(38, 17);
+            this.toolStripPixelInfo.Text = "R:G:B:";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
             // Imshow
             // 
             this.AccessibleDescription = "Imshow Preview";
-            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.ClientSize = new System.Drawing.Size(525, 373);
+            this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.toolStrip);
             this.Controls.Add(this.menuStrip);
             this.MainMenuStrip = this.menuStrip;
-            this.MaximizeBox = false;
             this.Name = "Imshow";
             this.ShowIcon = false;
-            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
             this.Text = "Imshow";
+            this.TopMost = true;
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +172,15 @@
         private System.Windows.Forms.MenuStrip menuStrip;
         private System.Windows.Forms.ToolStrip toolStrip;
         private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripPixelInfo;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- UI/UI/FormsLib/Imshow.cs
+++ UI/UI/FormsLib/Imshow.cs
@@ -3,22 +3,138 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FormsLib
 {
+    /// <summary>
+    /// Imshow Like interface to view images in dotnet
+    /// </summary>  
     public partial class Imshow : Form
     {
+        #region User32 dll imports
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+        #endregion
+
+        #region Property definitions
+        /// <summary>
+        /// get the color of the pixel under the mouse pointer
+        /// </summary>
+        Color mousePixelColor =Color.Black;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates new instance of the Imshow form
+        /// </summary>
         public Imshow()
         {
             InitializeComponent();
         }
-        public Imshow(Bitmap Image)
+        /// <summary>
+        /// Shows the Bitmap in a imshow like form
+        /// </summary>
+        /// <param name="image">Input bitmap to be previewed</param>
+        public Imshow(Bitmap image)
         {
             InitializeComponent();
+            pictureBox.Image = image;
         }
+        #endregion
+
+        #region EventHandlers
+
+        #region FormEventHandlers
+        /// <summary>
+        /// Tick handler
+        /// </summary>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //need work here for pixel location
+            mousePixelColor = GetColorAt(MousePosition.X, MousePosition.Y);
+            toolStripPixelInfo.Text = "X:" + MousePosition.X + ",Y:" + MousePosition.Y + "Red:" + mousePixelColor.R.ToString() + ",Green" + mousePixelColor.G.ToString() + ",Blue:" + mousePixelColor.B.ToString();
+            toolStripPixelInfo.BackColor = mousePixelColor;
+        }
+        #endregion
+
+        #region MenuStripClickHandlers
+        /// <summary>
+        /// Occurs when Open button is clicked on the menu tool strip
+        /// </summary>
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            open();
+        }
+        /// <summary>
+        /// Occurs when Open button is clicked on the menu tool strip
+        /// </summary>
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+        #endregion
+
+        #endregion
+
+        #region FunctionalMethods
+        /// <summary>
+        /// open a new image in the imshow form
+        /// </summary>
+        private void open()
+        {
+            //need work here
+            OpenFileDialog openDial = new OpenFileDialog();
+            openDial.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            string fileName = "";
+            if (openDial.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openDial.FileName;
+                pictureBox.Image = new Bitmap(fileName);
+            }
+        }
+        /// <summary>
+        /// Saves the existing image to the form
+        /// </summary>
+        private void Save()
+        {
+            //need work here
+            SaveFileDialog saveDial = new SaveFileDialog();
+            string fileName = "";
+            if (saveDial.ShowDialog() == DialogResult.OK)
+            {
+                fileName=saveDial.FileName;
+                new Bitmap(pictureBox.Image).Save(fileName, ImageFormat.Png);
+            }
+        }
+        /// <summary>
+        /// Gets the colour of the pixel located at the give point on the screen
+        /// </summary>
+        /// <param name="x">zero indexed pixel location from right to left</param>
+        /// <param name="y">zero indexed pixel location from top to bottom</param>
+        /// <returns></returns>
+        private Color GetColorAt(int x, int y)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            int a = (int)GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+            return Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
+        }
+        #endregion
+
     }
+
 }
